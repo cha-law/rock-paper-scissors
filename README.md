cb34d04b538460c120c09567724808f6319a7119
cha-law <char_law@outlook.com>
2024-12-31 17:29:21 +0000

    Added UI and changed code to work with the UI

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # rockpaperscissors
+
+Simple rock paper scissors for The Odin Project.
+
+Image Credit:
+
+Lime Stone icon by Icons8: https://icons8.com
+Paper Waste icon by Icons8 : https://icons8.com
+Scissors icon by Icons8: https://icons8.com
+Robot Icon by Icons8: https://icons8.com
+Human Icon by Icons8: https://icons8.com
--- rps.js
+++ rps.js
@@ -2,27 +2,39 @@
 
 // Returns rock, paper or scissors.
 function getComputerChoice() { 
+    let comImage = document.querySelector(".computer-img")
     result = Math.random() * 3
     if (result > 2) {
+        comImage.src = "./images/icons8-lime-stone-96.png"
         return "ROCK";
     } else if (result > 1) {
+        comImage.src = "./images/icons8-paper-waste-96.png"
         return "PAPER";
     } else {
+        comImage.src = "./images/icons8-scissors-96.png"
         return "SCISSORS";
     }
 }
 
 // Returns choice from the user.
-function getHumanChoice() {
-    choice = prompt("Choose from rock, paper, or scissors: ");
-
-    return choice;
+function getHumanChoice(target) {
+    let humanImage = document.querySelector(".human-img")
+    if ((target === document.querySelector("#rock")) || (target.parentNode === document.querySelector("#rock"))) {
+        humanImage.src = "./images/icons8-lime-stone-96.png"
+        return "ROCK";
+    } else if ((target === document.querySelector("#paper")) || (target.parentNode === document.querySelector("#paper"))) {
+        humanImage.src = "./images/icons8-paper-waste-96.png"
+        return "PAPER";
+    } else if ((target === document.querySelector("#scissors")) || (target.parentNode === document.querySelector("#scissors"))) {
+        humanImage.src = "./images/icons8-scissors-96.png"
+        return "SCISSORS";
+    } else {
+        return "INVALID";
+    }
 }
 
 // Plays one round of RPS.
 function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toUpperCase(); // Making the user input case-insensitive.
-
     if (humanChoice === computerChoice) { // Checking for a draw before other checks.
         return "DRAW";
         
@@ -60,40 +72,59 @@ function playGame() {
 
     let humanScore = 0;
     let computerScore = 0;
+    
+    let outcomeText = document.querySelector(".outcome-text")
+    let buttons = document.querySelectorAll("button");
 
-    // Loops the code 5 times.
-    for (let i = 1; i < 6; i++) {
+    let i = 1;
 
-        // Outputs the round number.
-        console.log(`Round ${i}.`);
+    // Getting the result of the round. (playing the round)
 
-        // Getting the result of the round. (playing the round)
-        let computerChoice = getComputerChoice()
-        result = playRound(getHumanChoice(), computerChoice);
+    document.onclick = function(event) {
 
-        // Checking the win condition, and outputting the corresponding message.
-        if (result === "WIN") {
-            console.log(`You won! The computer chose ${computerChoice}.`);
-            humanScore++;
-        } else if (result === "LOSE") {
-            console.log(`You lost. The computer chose ${computerChoice}`);
-            computerScore++;
-        } else if (result === "DRAW") {
-            console.log("It's a draw!");
+        if (i >= 5) {
+            document.querySelector(".round-text").textContent = `Final round.`;
+            endGame(humanScore, computerScore);
+            return;
+        }
+
+        let humanChoice = getHumanChoice(event.target)
+
+        if (humanChoice === "INVALID") {
+            return;
         } else {
-            console.log("Something went wrong.")
+            result = playRound(humanChoice, getComputerChoice());
+
+            // Checking the win condition, and outputting the corresponding message.
+            if (result === "WIN") {
+                outcomeText.textContent = `You won!`;
+                humanScore++;
+                document.querySelector(".human-score").textContent = String(humanScore + " points");
+            } else if (result === "LOSE") {
+                outcomeText.textContent = `You lost.`;
+                computerScore++;
+                document.querySelector(".computer-score").textContent = String(computerScore + " points");
+            } else if (result === "DRAW") {
+                outcomeText.textContent = "It's a draw!";
+            } else {
+                console.log("Something went wrong.");
+            }
+            document.querySelector(".round-text").textContent = `Round ${i}.`;
+            i++;
         }
     }
+}
 
+function endGame(humanScore, computerScore) {
     // The end of the game: collecting the scores and declaring a winner.
     if (humanScore === computerScore) {
-        console.log(`It was a draw. Good game.\nYou got ${humanScore} points.\nThe computer got ${computerScore} points.`)
+        document.querySelector(".outcome-text").textContent = `It was a draw. Good game.`;
     } else if (humanScore > computerScore) {
-        console.log(`You are the winner! Well done.\nYou got ${humanScore} points.\nThe computer got ${computerScore} points.`)
+        document.querySelector(".outcome-text").textContent = `You are the winner!`;
     } else {
-        console.log(`You lost. Better luck next time.\nYou got ${humanScore} points.\nThe computer got ${computerScore} points.`)
+        document.querySelector(".outcome-text").textContent = `You are a loser!`;
     }
-
+    return;
 }
 
 // Running the game.
